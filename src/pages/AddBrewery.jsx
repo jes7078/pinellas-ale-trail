121e7da79308dc7c038c50f7f95ce8e28106da81
Jeffrey Sojack <jeffrey.sojack@gmail.com>
2020-01-13 08:59:43 -0500

    recoded add,update,delete brewery

--- src/pages/AddBrewery.jsx
+++ src/pages/AddBrewery.jsx
@@ -3,128 +3,160 @@ import axios from 'axios'
 import Menu from '../components/Menu'
 
 const AddBrewery = () => {
-  const [breweryList, setBreweryList] = useState([])
-  const [breweryName, setBreweryName] = useState('')
-  const [breweryURL, setBreweryURL] = useState('')
-  const [breweryAddress, setBreweryAddress] = useState('')
-  const [breweryPhoneNumber, setBreweryPhoneNumber] = useState('')
-  const [breweryWebsite, setBreweryWebsite] = useState('')
+	const [ breweryList, setBreweryList ] = useState([])
+	const [ brewery, setBrewery ] = useState({
+		id: 0,
+		name: '',
+		url: '',
+		address: '',
+		phoneNumber: '',
+		website: ''
+	})
 
-  const enterName = eventData => {
-    if (eventData) {
-      setBreweryName(eventData.target.value)
-    }
-  }
+	const updateBreweryObject = (e) => {
+		e.persist()
+		setBrewery((prevBrewery) => ({
+			...prevBrewery,
+			[e.target.name]: e.target.value
+		}))
+	}
 
-  const enterBreweryURL = eventData => {
-    if (eventData) {
-      setBreweryURL(eventData.target.value)
-    }
-  }
+	const createBreweryList = async () => {
+		const resp = await axios.get('https://localhost:5001/api/Breweries')
+		setBreweryList(resp.data)
+	}
 
-  const enterBreweryAddress = eventData => {
-    if (eventData) {
-      setBreweryAddress(eventData.target.value)
-    }
-  }
+	const addIt = async (e) => {
+		e.preventDefault()
+		const resp = await axios.post(`https://localhost:5001/api/Breweries`, brewery)
+		if (resp.statusText === 'OK') {
+			window.alert('Brewery Added')
+		} else {
+			window.alert('Error, Brewery Not Added')
+		}
+		createBreweryList()
+	}
 
-  const enterBreweryPhoneNumber = eventData => {
-    if (eventData) {
-      setBreweryPhoneNumber(eventData.target.value)
-    }
-  }
+	const updateIt = async (e) => {
+		e.preventDefault()
+		const resp = await axios.put(`https://localhost:5001/api/Breweries/` + brewery.id, brewery)
+		if (resp.statusText === 'OK') {
+			window.alert('Brewery Changed')
+		} else {
+			window.alert('Error, Brewery Not Changed')
+		}
+		createBreweryList()
+	}
 
-  const enterBreweryWebsite = eventData => {
-    if (eventData) {
-      setBreweryWebsite(eventData.target.value)
-    }
-  }
+	const deleteit = async (id) => {
+		const resp = await axios.delete('https://localhost:5001/api/Breweries/' + id)
+		if (resp.statusText === 'OK') {
+			window.alert('Brewery deleted')
+		} else {
+			window.alert('Error, Brewery Not deleted')
+		}
+		createBreweryList()
+	}
 
-  const createBreweryList = async () => {
-    const resp = await axios.get('https://localhost:5001/api/Breweries')
-    setBreweryList(resp.data)
-  }
+	const selectIt = (id) => {
+		const filterBrewery = breweryList.filter((bre) => {
+			return bre.id === id
+		})
+		setBrewery({
+			id: filterBrewery[0].id,
+			name: filterBrewery[0].name,
+			url: filterBrewery[0].url,
+			address: filterBrewery[0].address,
+			phoneNumber: filterBrewery[0].phoneNumber,
+			website: filterBrewery[0].website
+		})
+	}
 
-  const submit = async () => {
-    let brewery = {
-      id: 0,
-      name: breweryName,
-      url: breweryURL,
-      address: breweryAddress,
-      phoneNumber: breweryPhoneNumber,
-      website: breweryWebsite,
-    }
-    const resp = await axios.post(
-      `https://localhost:5001/api/Breweries`,
-      brewery
-    )
-    if (resp.statusText === 'OK') {
-      window.alert('Brewery Added')
-    } else {
-      window.alert('Error, Brewery Not Added')
-    }
-    createBreweryList()
-  }
+	useEffect(() => {
+		createBreweryList()
+	}, [])
 
-  useEffect(() => {
-    createBreweryList()
-  }, [])
-  return (
-    <section>
-      <Menu />
-      <h1>Add a Brewery Page</h1>
-      <label>Name of Brewery</label>
-      <input
-        className="inputBar"
-        type="text"
-        value={breweryName}
-        placeholder="Enter Brewery Name"
-        onChange={enterName}
-      ></input>
-      <label>Brewery Pic URL</label>
-      <input
-        className="inputBar"
-        type="text"
-        value={breweryURL}
-        placeholder="Enter Brewery Pic URL"
-        onChange={enterBreweryURL}
-      ></input>
-      <label>Brewery Address</label>
-      <input
-        className="inputBar"
-        type="text"
-        value={breweryAddress}
-        placeholder="Enter Brewery Address"
-        onChange={enterBreweryAddress}
-      ></input>
-      <label>Brewery Phone Number</label>
-      <input
-        className="inputBar"
-        type="text"
-        value={breweryPhoneNumber}
-        placeholder="Enter Brewery Phone Number"
-        onChange={enterBreweryPhoneNumber}
-      ></input>
-      <label>Brewery Website Address</label>
-      <input
-        className="inputBar"
-        type="text"
-        value={breweryWebsite}
-        placeholder="Enter Brewery Website Address"
-        onChange={enterBreweryWebsite}
-      ></input>
-
-      <button className="submitButton" onClick={submit}>
-        Submit
-      </button>
-      <h1>Current Breweries</h1>
-      <ul>
-        {breweryList.map((brewery, index) => {
-          return <li key={index}>{brewery.name}</li>
-        })}
-      </ul>
-    </section>
-  )
+	return (
+		<section>
+			<Menu />
+			<h1 className="addBreweryTitle">Add, Update, or Delete a Brewery Page</h1>
+			<section className="addBreweryInputSection">
+				<form>
+					<label>Name of Brewery</label>
+					<input
+						name="name"
+						className="inputBar"
+						type="text"
+						value={brewery.Name}
+						placeholder="Enter Brewery Name"
+						onChange={updateBreweryObject}
+					/>
+					<label>Brewery Pic URL</label>
+					<input
+						name="url"
+						className="inputBar"
+						type="text"
+						value={brewery.URL}
+						placeholder="Enter Brewery Pic URL"
+						onChange={updateBreweryObject}
+					/>
+					<label>Brewery Address</label>
+					<input
+						name="address"
+						className="inputBar"
+						type="text"
+						value={brewery.Address}
+						placeholder="Enter Brewery Address"
+						onChange={updateBreweryObject}
+					/>
+					<label>Brewery Phone Number</label>
+					<input
+						name="phoneNumber"
+						className="inputBar"
+						type="text"
+						value={brewery.PhoneNumber}
+						placeholder="Enter Brewery Phone Number"
+						onChange={updateBreweryObject}
+					/>
+					<label>Brewery Website Address</label>
+					<input
+						name="website"
+						className="inputBar"
+						type="text"
+						value={brewery.Website}
+						placeholder="Enter Brewery Website Address"
+						onChange={updateBreweryObject}
+					/>
+					<section className="addBreweryButtons">
+						<button className="addButton" onClick={addIt}>
+							Add
+						</button>
+						<button className="updateButton" onClick={updateIt}>
+							Update
+						</button>
+						<button className="deleteButton" value={brewery.id} onClick={() => deleteit(brewery.id)}>
+							Delete
+						</button>
+					</section>
+				</form>
+			</section>
+			<h1 className="currentBreweriesTitle">Current Breweries</h1>
+			<section className="addCurrentBreweriesList">
+				<ul>
+					{breweryList.map((bre, index) => {
+						return (
+							<section key={index} className="addBreweryList">
+								<button value={bre.id} onClick={() => selectIt(bre.id)}>
+									Select for Update or Delete
+								</button>
+								<li key={index}>{bre.name}</li>
+							</section>
+						)
+					})}
+				</ul>
+			</section>
+		</section>
+	)
 }
 
 export default AddBrewery
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -2,50 +2,26 @@ import React from 'react'
 import Menu from '../components/Menu'
 import { Link } from 'react-router-dom'
 import AddBrewery from './AddBrewery'
-import UpdateBrewery from './UpdateBrewery'
-import DeleteBrewery from './DeleteBrewery'
 import AddStyle from './AddStyle'
-import UpdateStyle from './UpdateStyle'
-import DeleteStyle from './DeleteStyle'
 import AddBeer from './AddBeer'
-import UpdateBeer from './UpdateBeer'
-import DeleteBeer from './DeleteBeer'
 
 const AdminPage = () => {
 	return (
 		<section>
 			<Menu />
 
-			<h1>Admin Page</h1>
+			<h1 className="adminTitle">Admin Page</h1>
 			<section className="adminLinks">
 				<nav>
 					<ul>
 						<li>
-							<Link to="/AddBrewery">Add a Brewery</Link>
+							<Link to="/AddBrewery">Add, Update, or Delete a Brewery</Link>
 						</li>
 						<li>
-							<Link to="/UpdateBrewery">Update a Brewery</Link>
+							<Link to="/AddStyle">Add, Update, or Delete a Style</Link>
 						</li>
 						<li>
-							<Link to="/DeleteBrewery">Delete a Brewery</Link>
-						</li>
-						<li>
-							<Link to="/AddStyle">Add a Style</Link>
-						</li>
-						<li>
-							<Link to="/UpdateStyle">Update a Style</Link>
-						</li>
-						<li>
-							<Link to="/DeleteStyle">Delete a Style</Link>
-						</li>
-						<li>
-							<Link to="/AddBeer">Add a Beer</Link>
-						</li>
-						<li>
-							<Link to="/UpdateBeer">Update a Beer</Link>
-						</li>
-						<li>
-							<Link to="/DeleteBeer">Delete a Beer</Link>
+							<Link to="/AddBeer">Add, Update, or Delete a Beer</Link>
 						</li>
 					</ul>
 				</nav>
