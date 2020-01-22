551d6122fc50f15ca1b33dfbd66b5187be2c7cb9
Jeffrey Sojack <jeffrey.sojack@gmail.com>
2020-01-22 15:30:58 -0500

    changed admin to add info

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Menu = () => {
-	return (
-		<section className="menu">
-			<h1 id="menuTitle">The Pinellas Ale Trail</h1>
-			<section className="menuLinks">
-				<nav>
-					<ul>
-						<li>
-							<Link to="/">Home Page</Link>
-						</li>
-						<li>
-							<Link to="/BreweryMap">Brewery Map</Link>
-						</li>
-						<li>
-							<Link to="/AdminPage">Admin</Link>
-						</li>
-					</ul>
-				</nav>
-			</section>
-		</section>
-	)
+  return (
+    <section className="menu">
+      <h1 id="menuTitle">The Pinellas Ale Trail</h1>
+      <section className="menuLinks">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home Page</Link>
+            </li>
+            <li>
+              <Link to="/BreweryMap">Brewery Map</Link>
+            </li>
+            <li>
+              <Link to="/AdminPage">Add info</Link>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </section>
+  )
 }
 
 export default Menu
